6068729a4d7d3a3f76f2132ddfda22673086c146
KafetzisThomas <tomkafetzis06@gmail.com>
2023-01-03 15:14:27 +0200

    Refactor

--- simple_calculator.py
+++ simple_calculator.py
@@ -4,25 +4,41 @@
 # Project Title: Simple Calculator (https://github.com/KafetzisThomas/Simple-Calculator)
 # Author / Project Owner: KafetzisThomas (https://github.com/KafetzisThomas)
 
-def calculate():
-    a = float(input("\nWrite the first number: "))
-    b = float(input("Write the second number: "))
-    c = str(input("Write a symbol (+,-,*,/): "))
+class Calculator:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def addition(self):
+        print(self.x + self.y)
+    
+    def subtraction(self):
+        print(self.x - self.y)
+    
+    def multiplication(self):
+        print(self.x * self.y)
+    
+    def division(self):
+        print(self.x / self.y)
 
-    if c == "+":
-        d = a + b
-        print(f"Result: {d}")
-    elif c == "-":
-        d = a - b
-        print(f"Result: {d}")
-    elif c == "*":
-        d = a * b
-        print(f"Result: {d}")
-    elif c == "/":
-        d = a / b
-        print(f"Result: {d}")
-    else:
-        print("Undefined symbol. Try Again.")
-        calculate()
+a = float(input("\nWrite the first number: "))
+b = float(input("Write the second number: "))
 
-calculate()
+c = Calculator(a,b)
+print("\n1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = str(input("\nChoice (1-4): "))
+
+if(choice=="1"):
+    c.addition()
+elif(choice=="2"):
+    c.subtraction()
+elif(choice=="3"):
+    c.multiplication()
+elif(choice=="4"):
+    c.division()
+else:
+    print("Undefined choice.")
