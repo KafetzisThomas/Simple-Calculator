3a312cd43a5b550d21f4436ef2304eac69134d50
KafetzisThomas <tomkafetzis06@gmail.com>
2023-07-13 09:58:35 +0300

    Modify methods to return a value
    * Also, condition added for the times when the program is running directly by the python interpreter

--- main.py
+++ main.py
@@ -13,33 +13,33 @@ class Calculator:
 
   def addition(self):
     """Print a statement about adding two values"""
-    print(self.first_number + self.second_number)
+    return self.first_number + self.second_number
 
   def subtraction(self):
     """Print a statement about subtracting two values"""
-    print(self.first_number - self.second_number)
+    return self.first_number - self.second_number
   
   def multiplication(self):
     """Print a statement about multiplying two values"""
-    print(self.first_number * self.second_number)
+    return self.first_number * self.second_number
 
   def division(self):
     """Print a statement about dividing two values"""
-    print(self.first_number / self.second_number)
+    return self.first_number / self.second_number
 
-first_number = float(input("\nWrite first number: "))
-second_number = float(input("Write second number: "))
+if __name__ == '__main__':
+  first_number = float(input("\nWrite first number: "))
+  second_number = float(input("Write second number: "))
 
-c = Calculator(first_number, second_number)
-print("\n1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
+  calculator = Calculator(first_number, second_number)
+  print("\n1. Addition")
+  print("2. Subtraction")
+  print("3. Multiplication")
+  print("4. Division")
 
-choice = str(input("\nChoice (1-4): "))
-
-if choice == "1": c.addition()
-elif choice == "2": c.subtraction()
-elif choice == "3": c.multiplication()
-elif choice == "4": c.division()
-else: print("Undefined choice.")
+  choice = str(input("\nChoice (1-4): "))
+  if choice == "1": print(calculator.addition())
+  elif choice == "2": print(calculator.subtraction())
+  elif choice == "3": print(calculator.multiplication())
+  elif choice == "4": print(calculator.division())
+  else: print("Undefined choice.")
