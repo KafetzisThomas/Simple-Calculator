e45a5433444b3ff7a5f10d04381b591ad4a3c425
KafetzisThomas <tomkafetzis06@gmail.com>
2023-07-13 10:00:18 +0300

    Fix typo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-# Project Title: Simple Calculator (https://github.com/KafetzisThomas/Simple-Calculator)
+# Project Title: Simple-Calculator (https://github.com/KafetzisThomas/Simple-Calculator)
 # Author / Project Owner: KafetzisThomas (https://github.com/KafetzisThomas)
 
 class Calculator:
